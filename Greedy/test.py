90ec068643e6171f28ed1af13d7528db335ca936
SEOKWOOPARK <skkfea08@gmail.com>
2021-04-11 14:06:33 +0900

    Update test.py

--- Greedy/test.py
+++ Greedy/test.py
@@ -3,7 +3,6 @@ input = sys.stdin.readline
 i = 1
 
 while True:
-    l, p, v = map(int, input().split())
-    if l + p + v == 0:
-            break
-
+	l, p, v = map(int, input().split())
+	if l + p + v == 10:
+		print('hi')
