5429813c88dc8c5adb42a2c893173a8a4b56f160
SEOKWOOPARK <skkfea08@gmail.com>
2021-04-11 14:08:39 +0900

    Update test.py

--- Greedy/test.py
+++ Greedy/test.py
@@ -3,6 +3,6 @@ input = sys.stdin.readline
 i = 1
 
 while True:
-	l, p, v = map(int, input().split())
-	if l + p + v == 50:
-		print('hi')
+    l, p, v = map(int, input().split())
+    if l + p + v == 50:
+        print('hi')
