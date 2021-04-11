47006c88f7d332ee7f90e0e910c01ab51f88e096
SEOKWOOPARK <skkfea08@gmail.com>
2021-04-11 14:02:18 +0900

    Update test.py
    testing

--- Greedy/test.py
+++ Greedy/test.py
@@ -6,9 +6,7 @@ while True:
 	l, p, v = map(int, input().split())
 	if l + p + v == 0:
 		break
-	answer = (v // p) * l + min(v % p, 1)
-
-	print('Case %d %d' %(i, answer))
+	answer = (v // p) * l + min(v % p, l)
+	print('hello');
 	i += 1
 
-	
