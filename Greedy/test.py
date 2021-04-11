473296359ae5ad12c8f39b7f1d26886afb74e6ef
SEOKWOOPARK <skkfea08@gmail.com>
2021-04-11 14:04:35 +0900

    Update test.py
    testing

--- Greedy/test.py
+++ Greedy/test.py
@@ -3,10 +3,7 @@ input = sys.stdin.readline
 i = 1
 
 while True:
-	l, p, v = map(int, input().split())
-	if l + p + v == 0:
-		break
-	answer = (v // p) * l + min(v % p, l)
-	print('hello');
-	i += 1
+  l, p, v = map(int, input().split())
+  if l + p + v == 0:
+    break
 
