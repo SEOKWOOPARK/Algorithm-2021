a548152299abb8cd638086bc43432bb58b15cda0
SEOKWOOPARK <skkfea08@gmail.com>
2021-04-12 15:27:56 +0900

    tab 수정

--- BinarySearch/kNumber_1300.py
+++ BinarySearch/kNumber_1300.py
@@ -9,16 +9,17 @@ high = target
 answer = 0
 
 while low <= high:
-	m = (low + high) // 2
-	count = 0
-	for i in range(1, n + 1):
-		count = count + min(m // i, n)
 
-	if count < target:
-		low = m + 1
-	else:
-		answer = m
-		high = m - 1
+    m = (low + high) // 2
+    count = 0
+    for i in range(1, n + 1):
+        count = count + min(m // i, n)
+
+    if count < target:
+        low = m + 1
+    else:
+        answer = m
+        high = m - 1
 
 print(answer)
 
