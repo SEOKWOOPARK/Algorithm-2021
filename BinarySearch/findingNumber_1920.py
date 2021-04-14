1cb3ec8815657427813abd5d49f34781a25ac603
SEOKWOOPARK <skkfea08@gmail.com>
2021-04-14 20:24:12 +0900

    탭 수정
    탭 수정

--- BinarySearch/findingNumber_1920.py
+++ BinarySearch/findingNumber_1920.py
@@ -7,21 +7,21 @@ m = int(input())
 M = list(map(int, input().split()))
 
 def binarySearch(target, N, start, end):
-	if start > end:
-		return 0
-	m = (start + end) // 2
+    if start > end:
+        return 0
+    m = (start + end) // 2
 
-	if target == N[m]:
-		return 1
-	elif target < N[m]:
-		return binarySearch(target, N, start, m - 1)
-	else:
-		return binarySearch(target, N, m + 1, end)
+    if target == N[m]:
+        return 1
+    elif target < N[m]:
+        return binarySearch(target, N, start, m - 1)
+    else:
+        return binarySearch(target, N, m + 1, end)
 
 for target in M:
-	start = 0
-	end = len(N) - 1
-	print(binarySearch(target, N, start, end))
+    start = 0
+    end = len(N) - 1
+    print(binarySearch(target, N, start, end))
 
 # 숫자카드(10815) 문제와 매우 유사하다
 # 기본적인 이분탐색 문제
--- BinarySearch/installingRouter_2110.py
+++ BinarySearch/installingRouter_2110.py
@@ -7,24 +7,24 @@ ap.sort()
 answer = 0
 
 def solve(minimum , maximum):
-	if minimum > maximum:
-		return
+    if minimum > maximum:
+        return
 
-	dist = (maximum + minimum) // 2
-	cnt = 1
-	target = 0
+    dist = (maximum + minimum) // 2
+    cnt = 1
+    target = 0
 
-	for idx in range(N):
-		if ap[idx] >= ap[target] + dist:
-			cnt += 1
-			target = idx
+    for idx in range(N):
+        if ap[idx] >= ap[target] + dist:
+            cnt += 1
+            target = idx
 
-	if cnt >= C:
-		global answer
-		answer = max(answer, dist)
-		solve(dist + 1, maximum) # cnt가 같거나 큰 경우 중간값 이후부터 끝까지 탐색
-	else: 
-		solve(minimum, dist - 1) # cnt가 더 작으면 처음부터 중간값 이전까지 탐색
+    if cnt >= C:
+        global answer
+        answer = max(answer, dist)
+        solve(dist + 1, maximum) # cnt가 같거나 큰 경우 중간값 이후부터 끝까지 탐색
+    else: 
+        solve(minimum, dist - 1) # cnt가 더 작으면 처음부터 중간값 이전까지 탐색
 
 solve(1, (ap[-1] - ap[0]) // (C - 1) + 1)
 # 1부터 5(주어진 시작값 1과 9의 중간값)까지 max,min으로 설정
