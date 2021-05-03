bb145a38f8efb777caf07bd5a4e359e52fe236bf
SEOKWOOPARK <skkfea08@gmail.com>
2021-05-03 11:06:25 +0900

    2020 kakao
    실패 후 '가사 검색' 문제  솔루션

--- kakao/2020_searchingLyric.py
+++ kakao/2020_searchingLyric.py
@@ -31,3 +31,72 @@ def solution(words, queries):
     
     return answer
 
+# -------------------------------------------------------
+
+from collections import defaultdict
+
+# Trie 자료구조안에 위치하고 있는 노드 정의
+class Node(object):
+    def __init__(self, key, passnumber=None, isEnd=None):
+        self.key = key
+        # 해당 노드를 지나간 길이 별 단어 갯수
+        self.passnumber = defaultdict(int)  
+        self.isEnd = False
+        self.children = {}
+
+# Trie(문자열 + 트리) 자료구조 활용
+class Trie(object):
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, string):
+        curNode = self.head
+        curNode.passnumber[len(string)] += 1
+        for char in string:
+            if char not in curNode.children:
+                curNode.children[char] = Node(char)
+
+            curNode = curNode.children[char]
+            curNode.passnumber[len(string)] += 1
+
+        curNode.isEnd = True
+
+    def search(self, query):
+        curNode = self.head
+        for q in query:
+            if q == "?":
+                break
+            if q in curNode.children:
+                curNode = curNode.children[q]
+            else:
+                return 0
+
+        return curNode.passnumber[len(query)]
+
+
+def solution(words, queries):
+    trie = Trie()
+    r_trie = Trie()
+    r_words = [w[::-1] for w in words]
+    dic = {}
+    answer = []
+
+    for word in words:
+        trie.insert(word)
+    for word in r_words:
+        r_trie.insert(word)
+    for query in queries:
+        if query in dic:
+            answer.append(dic[query])
+            continue
+        if query.endswith("?"):
+            result = trie.search(query)
+            answer.append(result)
+            dic["query"] = result
+
+        else:
+            result = r_trie.search(query[::-1])
+            answer.append(result)
+            dic["query"] = result
+
+    return answer
